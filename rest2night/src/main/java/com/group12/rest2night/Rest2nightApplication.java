a5133fce05bade851451b5bbf1a264acbb8370ac
Usmon <usmonpahlavonov@gmail.com>
2024-04-22 15:04:01 +0900

    init

---
+++ rest2night/src/main/java/com/group12/rest2night/Rest2nightApplication.java
@@ -0,0 +1,13 @@
+package com.group12.rest2night;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest2nightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rest2nightApplication.class, args);
+	}
+
+}
---
+++ rest2night/src/test/java/com/group12/rest2night/Rest2nightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group12.rest2night;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest2nightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
