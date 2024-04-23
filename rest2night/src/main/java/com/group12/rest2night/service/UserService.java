604a181fbb346d1b634771e3febb30bfd0f10d80
Usmon <usmonpahlavonov@gmail.com>
2024-04-23 20:14:19 +0900

    bug fix

--- rest2night/src/main/java/com/group12/rest2night/service/UserService.java
+++ rest2night/src/main/java/com/group12/rest2night/service/UserService.java
@@ -1,12 +1,15 @@
 package com.group12.rest2night.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.group12.rest2night.entity.User;
 
 import com.group12.rest2night.repository.UserRepository;
 import org.bson.types.ObjectId;
 import java.util.List;
 
+@Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
