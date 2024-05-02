0bf03405f17e59f55757fdfd08867711f20004b3
Usmon <usmonpahlavonov@gmail.com>
2024-05-02 16:42:58 +0900

    bug fix

--- rest2night/src/main/java/com/group12/rest2night/entity/User.java
+++ rest2night/src/main/java/com/group12/rest2night/entity/User.java
@@ -8,9 +8,6 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-
 import java.util.List;
 
 @Data
